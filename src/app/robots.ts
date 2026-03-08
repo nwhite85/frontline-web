940ecaf07d0009279db8d2712d59dde07389ce56
Nick White <nwhite@Nicks-Mac-mini.local>
2026-03-08 18:14:59 +0000

    Free tee: rename to /free-tee, add product image, update copy

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -17,6 +17,7 @@ export default function robots(): MetadataRoute.Robots {
           '/signup',
           '/founder-merch',
           '/merch',
+          '/free-tee',
         ],
       },
     ],
